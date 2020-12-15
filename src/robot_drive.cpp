326968a686a398a9f1655ef4e369cef434abd2bd
kelvin34501 <kelvin34501@gmail.com>
2020-12-15 20:09:08 +0800

    fail on omni wheel

--- src/robot_drive.cpp
+++ src/robot_drive.cpp
@@ -2,6 +2,19 @@
 #include <gazebo/msgs/msgs.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/transport/transport.hh>
+#include <string>
+#include <unordered_map>
+
+namespace zz {
+enum class Wheel {
+    FrontLeft,
+    FrontRight,
+    RearLeft,
+    RearRight,
+};
+}
+
+using zz::Wheel;
 
 namespace gazebo {
 /// \brief A plugin to control a Velodyne sensor.
@@ -29,8 +42,23 @@ class RobotDrive : public ModelPlugin {
         // store the model pointer for convenience
         this->model = _model;
 
-        // get the first joint. We are making an assumption about the model having one joint that is the rotation joint
-        this->joint = _model->GetJoints()[0];
+// get the first joint. We are making an assumption about the model having one joint that is the rotation joint
+#define BASE_INDEX (32)
+        auto joints                            = _model->GetJoints();
+        this->joint_mapping[Wheel::FrontLeft]  = joints[BASE_INDEX + 0];
+        this->joint_mapping[Wheel::RearLeft]   = joints[BASE_INDEX + 1];
+        this->joint_mapping[Wheel::FrontRight] = joints[BASE_INDEX + 2];
+        this->joint_mapping[Wheel::RearRight]  = joints[BASE_INDEX + 3];
+
+        this->pid_mapping[Wheel::FrontLeft]  = common::PID(0.1, 0, 0);
+        this->pid_mapping[Wheel::RearLeft]   = common::PID(0.1, 0, 0);
+        this->pid_mapping[Wheel::FrontRight] = common::PID(0.1, 0, 0);
+        this->pid_mapping[Wheel::RearRight]  = common::PID(0.1, 0, 0);
+
+        std::cout << "[Joint] FrontLeft: " << this->joint_mapping[Wheel::FrontLeft]->GetScopedName() << '\n';
+        std::cout << "[Joint] RearLeft: " << this->joint_mapping[Wheel::RearLeft]->GetScopedName() << '\n';
+        std::cout << "[Joint] FrontRight: " << this->joint_mapping[Wheel::FrontRight]->GetScopedName() << '\n';
+        std::cout << "[Joint] RearRight: " << this->joint_mapping[Wheel::RearRight]->GetScopedName() << '\n';
 
         double velocity = 0.0;
 
@@ -40,7 +68,20 @@ class RobotDrive : public ModelPlugin {
         }
 
         // apply the p-controller to the joint
-        this->model->GetJoint(this->joint->GetScopedName())->SetVelocity(0, velocity);
+        this->model->GetJointController()->SetVelocityPID(this->joint_mapping[Wheel::FrontLeft]->GetScopedName(),
+                                                          this->pid_mapping[Wheel::FrontLeft]);
+        this->model->GetJointController()->SetVelocityPID(this->joint_mapping[Wheel::RearLeft]->GetScopedName(),
+                                                          this->pid_mapping[Wheel::RearLeft]);
+        this->model->GetJointController()->SetVelocityPID(this->joint_mapping[Wheel::FrontRight]->GetScopedName(),
+                                                          this->pid_mapping[Wheel::FrontRight]);
+        this->model->GetJointController()->SetVelocityPID(this->joint_mapping[Wheel::RearRight]->GetScopedName(),
+                                                          this->pid_mapping[Wheel::RearRight]);
+
+        this->model->GetJointController()->SetVelocityTarget(this->joint_mapping[Wheel::FrontLeft]->GetScopedName(), 0);
+        this->model->GetJointController()->SetVelocityTarget(this->joint_mapping[Wheel::RearLeft]->GetScopedName(), 0);
+        this->model->GetJointController()->SetVelocityTarget(this->joint_mapping[Wheel::FrontRight]->GetScopedName(),
+                                                             0);
+        this->model->GetJointController()->SetVelocityTarget(this->joint_mapping[Wheel::RearRight]->GetScopedName(), 0);
 
         // Create the node
         this->node = transport::NodePtr(new transport::Node());
@@ -62,9 +103,12 @@ class RobotDrive : public ModelPlugin {
     /// \brief Set the velocity of the Velodyne
     /// \param[in] _vel New target velocity
     public:
-    void SetVelocity(const double& _vel) {
+    void SetVelocity(const double _vel) {
         // Set the joint's target velocity.
-        this->model->GetJoint(this->joint->GetScopedName())->SetVelocity(0, _vel);
+        this->joint_mapping[Wheel::FrontLeft]->SetVelocity(0, _vel);
+        this->joint_mapping[Wheel::RearLeft]->SetVelocity(0, _vel);
+        this->joint_mapping[Wheel::FrontRight]->SetVelocity(0, _vel);
+        this->joint_mapping[Wheel::RearRight]->SetVelocity(0, _vel);
     }
 
     /// \brief Pointer to the model.
@@ -73,7 +117,11 @@ class RobotDrive : public ModelPlugin {
 
     /// \brief Pointer to the joint.
     private:
-    physics::JointPtr joint;
+    std::unordered_map<Wheel, physics::JointPtr> joint_mapping;
+
+    /// \brief PID
+    private:
+    std::unordered_map<Wheel, common::PID> pid_mapping;
 
     /// \brief A node used for transport
     private:
--- src/world_init.cpp
+++ src/world_init.cpp
@@ -1,128 +1,111 @@
 // this plugin initialize the world
 
 #include "rapidjson/document.h"
-#include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
 
-#include "ignition/math/Pose3.hh"
-#include "gazebo/physics/physics.hh"
 #include "gazebo/common/common.hh"
 #include "gazebo/gazebo.hh"
+#include "gazebo/physics/physics.hh"
+#include "ignition/math/Pose3.hh"
 
-#include <vector>
 #include <iostream>
 #include <sstream>
+#include <vector>
 
-using std::vector;
 using std::cout;
+using std::vector;
 
-namespace gazebo
-{
-    class WorldInit: public WorldPlugin {
-        public: void Load(physics::WorldPtr _parent, sdf::ElementPtr) {
-            // task: insert things from string
+namespace gazebo {
+class WorldInit : public WorldPlugin {
+    public:
+    void Load(physics::WorldPtr _parent, sdf::ElementPtr) {
+        // task: insert things from string
 
-            // ========================================================================================================
-            vector<vector<double>> ob_pos {
-                {5, 2.8},
-                {5.5, 2.5},
-                {6.3, 2.4},
-                {6.8, 2.2},
-                {7.1, 2},
-                {7.4, 1.8},
-                {7.6, 1.6},
-                {7.9, 1.2},
-                {8.2, 1},
-                {8.4, 0.8},
-                {8.5, 0.6}
-            };
-            
-            // insert a box:
-            int ob_id = 0;
-            for (const auto &pos: ob_pos) {
-                std::cout << "======\n";
-                std::cout << "Placing Box: " << pos[0] << ' ' << pos[1] << '\n';
+        // ========================================================================================================
+        vector<vector<double>> ob_pos{{5, 2.8},   {5.5, 2.5}, {6.3, 2.4}, {6.8, 2.2}, {7.1, 2},  {7.4, 1.8},
+                                      {7.6, 1.6}, {7.9, 1.2}, {8.2, 1},   {8.4, 0.8}, {8.5, 0.6}};
 
-                sdf::SDF ob_sdf;
-                std::stringstream sdf_ss;
+        // insert a box:
+        int ob_id = 0;
+        for (const auto& pos : ob_pos) {
+            std::cout << "======\n";
+            std::cout << "Placing Box: " << pos[0] << ' ' << pos[1] << '\n';
 
-                sdf_ss << "<sdf version='1.4'>\n";
-                sdf_ss << "    <model name='ob_" << ob_id++ << "'>\n";
-                sdf_ss << "        <pose>" << pos[0] << ' ' << pos[1] <<" 0 0 0 0</pose>\n";
-                sdf_ss << "        <link name ='link'>\n";
-                sdf_ss << "            <pose>0 0 0.25 0 0 0</pose>\n";
-                sdf_ss << "            <collision name ='collision'>\n";
-                sdf_ss << "                <geometry>\n";
-                sdf_ss << "                    <box><size>0.2 0.2 0.5</size></box>\n";
-                sdf_ss << "                </geometry>\n";
-                sdf_ss << "            </collision>\n";
-                sdf_ss << "            <visual name ='visual'>\n";
-                sdf_ss << "                <geometry>\n";
-                sdf_ss << "                    <box><size>0.2 0.2 0.5</size></box>\n";
-                sdf_ss << "                </geometry>\n";
-                sdf_ss << "            </visual>\n";
-                sdf_ss << "        </link>\n";
-                sdf_ss << "    </model>\n";
-                sdf_ss << "</sdf>";
+            sdf::SDF ob_sdf;
+            std::stringstream sdf_ss;
 
-                std::cout << "\tThe SDF:\n" << sdf_ss.str() << '\n';
+            sdf_ss << "<sdf version='1.4'>\n";
+            sdf_ss << "    <model name='ob_" << ob_id++ << "'>\n";
+            sdf_ss << "        <pose>" << pos[0] << ' ' << pos[1] << " 0 0 0 0</pose>\n";
+            sdf_ss << "        <link name ='link'>\n";
+            sdf_ss << "            <pose>0 0 0.25 0 0 0</pose>\n";
+            sdf_ss << "            <collision name ='collision'>\n";
+            sdf_ss << "                <geometry>\n";
+            sdf_ss << "                    <box><size>0.2 0.2 0.5</size></box>\n";
+            sdf_ss << "                </geometry>\n";
+            sdf_ss << "            </collision>\n";
+            sdf_ss << "            <visual name ='visual'>\n";
+            sdf_ss << "                <geometry>\n";
+            sdf_ss << "                    <box><size>0.2 0.2 0.5</size></box>\n";
+            sdf_ss << "                </geometry>\n";
+            sdf_ss << "            </visual>\n";
+            sdf_ss << "        </link>\n";
+            sdf_ss << "    </model>\n";
+            sdf_ss << "</sdf>";
 
-                ob_sdf.SetFromString(sdf_ss.str());
-                // sdf::ElementPtr ob_model = ob_sdf.Root()->GetElement("model");
+            std::cout << "\tThe SDF:\n" << sdf_ss.str() << '\n';
 
-                _parent->InsertModelSDF(ob_sdf);
+            ob_sdf.SetFromString(sdf_ss.str());
+            // sdf::ElementPtr ob_model = ob_sdf.Root()->GetElement("model");
 
-                std::cout << "<<<<<<\n";
-            }
-            // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+            _parent->InsertModelSDF(ob_sdf);
 
-            // ========================================================================================================
-            vector<vector<double>> robot_pos {
-                {-1, 6, 0},
-                {-2, 6, 0},
-                {-3, 6, 0},
-                {-4, 6, 0},
-                {0, 6, 0}
-            };
-            int fol_num = 4;
-            int N = robot_pos.size();
-
-            // insert robots
-            int robot_id = 0;
-            for (const auto &pos: robot_pos) {
-                std::cout << "======\n";
-                bool is_follower = (robot_id < fol_num);
-
-                if (is_follower) {
-                    std::cout << "Placing Robot (follower): " << pos[0] << ' ' << pos[1] << '\n';
-                } else {
-                    std::cout << "Placing Robot (leader): " << pos[0] << ' ' << pos[1] << '\n';
-                }
-
-                sdf::SDF robot_sdf;
-                std::stringstream sdf_ss;
-
-                sdf_ss << "<sdf version='1.4'>\n";
-                sdf_ss << "    <model name='robot_" << robot_id++ << "'>\n";
-                sdf_ss << "        <pose>" << pos[0] << ' ' << pos[1] <<" 0 0 0 0</pose>\n";
-                sdf_ss << "        <include><uri>model://robot_2</uri></include>\n";
-                sdf_ss << "        <plugin name='velodyne_control' filename='libvelodyne_plugin.so'>\n";
-                sdf_ss << "            <velocity>250.0</velocity>\n";
-                sdf_ss << "        </plugin>\n";
-                sdf_ss << "    </model>\n";
-                sdf_ss << "</sdf>";
-
-                std::cout << "\tThe SDF:\n" << sdf_ss.str() << '\n';
-
-                robot_sdf.SetFromString(sdf_ss.str());
-
-                _parent->InsertModelSDF(robot_sdf);
-
-                std::cout << "<<<<<<\n";
-                // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-            }
+            std::cout << "<<<<<<\n";
         }
-    };
+        // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-    GZ_REGISTER_WORLD_PLUGIN(WorldInit)
+        // ========================================================================================================
+        vector<vector<double>> robot_pos{{-1, 6, 0}, {-2, 6, 0}, {-3, 6, 0}, {-4, 6, 0}, {0, 6, 0}};
+        int fol_num = 4;
+        int N       = robot_pos.size();
+
+        // insert robots
+        int robot_id = 0;
+        for (const auto& pos : robot_pos) {
+            std::cout << "======\n";
+            bool is_follower = (robot_id < fol_num);
+
+            if (is_follower) {
+                std::cout << "Placing Robot (follower): " << pos[0] << ' ' << pos[1] << '\n';
+            } else {
+                std::cout << "Placing Robot (leader): " << pos[0] << ' ' << pos[1] << '\n';
+            }
+
+            sdf::SDF robot_sdf;
+            std::stringstream sdf_ss;
+
+            sdf_ss << "<sdf version='1.7'>\n";
+            sdf_ss << "    <model name='robot_" << robot_id++ << "'>\n";
+            sdf_ss << "        <pose>" << pos[0] << ' ' << pos[1] << " 0 0 0 0</pose>\n";
+            sdf_ss << "        <include><uri>model://robot_model_4</uri></include>\n";
+            sdf_ss << "        <plugin name='robot_drive' filename='libproject_robot_drive.so'>\n";
+            sdf_ss << "            <velocity>5.0</velocity>\n";
+            sdf_ss << "        </plugin>\n";
+            sdf_ss << "    </model>\n";
+            sdf_ss << "</sdf>";
+
+            std::cout << "\tThe SDF:\n" << sdf_ss.str() << '\n';
+
+            robot_sdf.SetFromString(sdf_ss.str());
+
+            _parent->InsertModelSDF(robot_sdf);
+
+            std::cout << "<<<<<<\n";
+            // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        }
+    }
+};
+
+GZ_REGISTER_WORLD_PLUGIN(WorldInit)
 } // namespace gazebo
